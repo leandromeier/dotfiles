1eec8595d4d6bf234cd0550329ccdc56ea52241a
Leandro <leandromeier.nb@gmail.com>
2023-02-09 19:07:16 +0100

    added scripts for bg management, added exec script statements

---
+++ sway/wallpaper-dynamic.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+swaybg -i $(find ~/Desktop/background/. -type f | shuf -n1) -m fill &
+OLD_PID=$!
+while true; do
+    sleep 10
+    swaybg -i $(find ~/Desktop/background/. -type f | shuf -n1) -m fill &
+    NEXT_PID=$!
+    sleep 5
+    kill $OLD_PID
+    OLD_PID=$NEXT_PID
+done
