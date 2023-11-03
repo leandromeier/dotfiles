3e85df2570a977c645f41fb8f7d3168f35e287d6
Leandro Meier <leandromeier.nb@gmail.com>
2023-11-03 16:07:13 +0100

    now even better

--- scripts/external-brightness.sh
+++ scripts/external-brightness.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 key=$1
-if [ 0 -lt $key ] && [ $key -lt 100 ]; then 
-  ddcutil setvcp 10 $key
-else
-  echo "invalid value"
-fi
+# h is passed to query current brightness
+# valid options are integers between 0 and 100
+#
+case $key in
+    h ) ddcutil getvcp 10 ;;
+    ''|*[!0-9]*) echo invalid value ;;
+    *)  if [ 0 -lt $key ] && [ $key -lt 100 ]; then 
+          ddcutil setvcp 10 $key
+        else
+          echo invalid value
+    fi
+esac
+
