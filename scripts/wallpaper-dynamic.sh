f48bd66f167f215febec569ec44bb5cc78dd0ea5
Leandro Meier <leandromeier.nb@gmail.com>
2024-01-16 15:06:46 +0100

    updated sway config and scripts

---
+++ scripts/wallpaper-dynamic.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+swaybg -i $(find ~/Documents/background/. -type f | shuf -n1) -m fit &
+OLD_PID=$!
+while true; do
+  # time is counted in seconds
+    sleep 600
+    swaybg -i $(find ~/Documents/background/. -type f | shuf -n1) -m fit &
+    NEXT_PID=$!
+    sleep 1
+    kill $OLD_PID
+    OLD_PID=$NEXT_PID
+done
---
+++ scripts/wallpaper-fresh-random.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+swaybg -i $(find ~/Documents/background/. -type f | shuf -n1) -m fit &
