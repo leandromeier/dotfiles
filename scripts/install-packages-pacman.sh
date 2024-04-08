4a72ecb841d6cd829e45518308d1166bc495e6cf
Leandro Meier <leandromeier.nb@gmail.com>
2024-04-08 13:57:33 +0200

    added scripts to install commonly used packages

---
+++ scripts/install-packages-pacman.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 7z
+sudo pacman -S thunderbird firefox alacritty fish lsd neovim fuzzel rofi dmenu sagemath ranger texlive zathura zathura-pdf-mupdf steam sway swaybg swayidle waybar grim slurp wl-clipboard gimp dolphin mpv vlc mirage rclone slimbookbattery joplin discord pavucontrol blueman bluez
