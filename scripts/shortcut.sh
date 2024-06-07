cd743c2b05c257dcc64890495e12b0661eee60dc
Leandro Meier <leandromeier.nb@gmail.com>
2024-06-07 15:21:50 +0200

    deleted old stuff, removed commented out bits, added packages to lists

--- scripts/shortcut.sh
+++ scripts/shortcut.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
+#heavily pruned (original by Gilles Castel)
 
-root="$(dirname "$(realpath "$0")")"
 key="$1"
-terminal="alacritty -e fish -i -c"
-vim="nvim"
-node="/usr/bin/node"
-
-# open_xournalpp () {
-#     cd ~/math/notes/daily-notes/$(date +"%F"); 
-#     if [ -f "note.xopp" ]; then
-#         xournalpp note.xopp;
-#     else 
-#         cp ~/math/scripts/template.xopp note.xopp;
-#         xournalpp note.xopp;
-#     fi
-# }
-
 case $key in
    a ) cd ~/math/articles;
        pdf_file="$(ls . | rofi -i -dmenu)";
@@ -27,9 +13,3 @@ case $key in
        [ -f "$pdf_file" ] && zathura "$(realpath "$pdf_file")" || firefox "https://duckduckgo.com/?hps=1&q=$pdf_file" ;;
 esac
 
-   #r ) $terminal "ranger ~/math/notes/daily-notes/$(date +"%F")" ;;
-   #o ) zathura ~/math/notes/daily-notes/main.pdf;;
-   #f ) $node $root/copy-pdf-reference.js ;;
-   #t ) $terminal "cd ~/math/notes/daily-notes; $vim ~/math/notes/daily-notes/main.tex" ;;
-   #n ) $terminal "cd ~/math/notes/daily-notes/$(date +"%F"); $vim ~/math/notes/daily-notes/$(date +"%F")/note.tex" ;;
-       #[ -f "$pdf_file" ] && zathura "$(realpath "$pdf_file")" || firefox "https://google.com/search?q=$pdf_file" ;;
