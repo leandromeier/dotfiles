df462801cfb26aa8b4c2a825d12dd92028f646ec
Leandro Meier <leandromeier.nb@gmail.com>
2025-01-07 17:25:00 +0100

    added jupyter-notebook

--- scripts/install-packages-pacman.sh
+++ scripts/install-packages-pacman.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo pacman -S firefox alacritty fish lsd neovim fuzzel rofi dmenu sagemath ranger texlive-core texlive-science texlive-latexextra texlive texlive-bin zathura zathura-pdf-mupdf steam sway swaybg swayidle waybar grim slurp wl-clipboard gimp dolphin mpv vlc rclone discord pavucontrol blueman bluez wl-clipboard brightnessctl thunderbird python-pip python-pynvim snapd texlive-binextra texlive-langgerman gphoto2 ddcutil geeqie
+sudo pacman -S firefox alacritty fish lsd neovim fuzzel rofi dmenu sagemath ranger texlive-core texlive-science texlive-latexextra texlive texlive-bin zathura zathura-pdf-mupdf steam sway swaybg swayidle waybar grim slurp wl-clipboard gimp dolphin mpv vlc rclone discord pavucontrol blueman bluez wl-clipboard brightnessctl thunderbirdpython3 python-pip python-pynvim snapd texlive-binextra texlive-langgerman gphoto2 ddcutil geeqie jupyter-notebook jupyter-core jupyter-client
 
