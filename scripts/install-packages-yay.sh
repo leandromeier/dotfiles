b79a890eaa165a181bc7af29208711a54a552ec2
Leandro Meier <leandromeier.nb@gmail.com>
2025-03-14 13:36:46 +0100

    updated to -Syu

--- scripts/install-packages-yay.sh
+++ scripts/install-packages-yay.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yay -S youtube-dl-gui linux-steam-integration signal-desktop swaylock-effects joplin-desktop
+yay -Syu youtube-dl-gui linux-steam-integration signal-desktop swaylock-effects joplin-desktop
