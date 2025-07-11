2dfb05101c7dba63f81470a0b44881054400c9e6
Leandro Meier <leandromeier.nb@gmailcom>
2025-07-11 09:02:01 +0200

    added ssh support to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,18 @@ source ~/.config/fish/aliases-public.fish
 
 #set neovim as default editor
 set -gx EDITOR nvim
+
+
+## try to get ssh to work better
+if status is-login
+    keychain --clear --quiet
+end
+
+# don't have any gpg keys
+# if test -f ~/.keychain/(hostname)-gpg-fish
+#     source ~/.keychain/(hostname)-gpg-fish
+# end
+
+if test -f ~/.keychain/(hostname)-fish
+    source ~/.keychain/(hostname)-fish
+end
