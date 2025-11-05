97a5b770654063f694189eebbfd61581213a62ec
Leandro Meier <leandromeier.nb@gmail.com>
2025-11-05 14:19:52 +0100

    added custom colors and keybindings for xpp-vi-plugin

---
+++ xpp/colors.lua
@@ -0,0 +1,13 @@
+-- change the hex values to customize colors
+blackColor = 0x000000
+whiteColor = 0xf8f8f2
+pinkColor = 0xff79c6
+redColor = 0xff5555
+orangeColor = 0xffb86c
+yellowColor = 0xf1fa8c
+greenColor = 0x199125
+lightGreenColor = 0x40EB21
+-- greenColor = 0x50fa7b
+cyanColor = 0x8be9fd
+blueColor = 0x6272a4
+purpleColor = 0xbd93f9
